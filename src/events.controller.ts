ed8ffd079537b6ad35c170be7cfc4dd8b3426517
FEDYCHUNG <fedychung@gmail.com>
2023-04-25 21:14:55 +0700

    create crud event-controller

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,6 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Param,
+    Body,
+    HttpCode,
+} from '@nestjs/common';
 
-@Controller()
-export class EventController{
+@Controller('/events')
+export class EventController {
+    @Get()
+    findAll() {
+        return [
+            { id: 1, name: 'First Event' },
+            { id: 2, name: 'Second Event' },
+        ];
+    }
 
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return id;
+    }
+
+    @Post()
+    create(@Body() input) {
+        return input;
+    }
+
+    @Patch(':id')
+    udpate(@Param('id') id, @Body() input) {
+        return input;
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    remove(@Param('id') id) {}
 }
